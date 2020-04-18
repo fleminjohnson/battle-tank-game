ce7e7900f5a24e3ff5ac876d6f34d86fe9f81365
Flemin Johnson <fjfleminjohnson@gmail.com>
2020-04-18 09:54:37 -0700

    Implemented Singleton

---
+++ Assets/Script/TankService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankService : SingletonBehaviour<TankService>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
